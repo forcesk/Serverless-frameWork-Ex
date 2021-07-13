82d0781a43d9ea19b5abf2b17d222393a7778776
Emmanuel Castillo <emm.castillog@gmail.com>
2021-07-13 16:38:17 -0500

    se eliminó el endpoint dee ejemplo

--- handler.py
+++ handler.py
@@ -1,20 +1,5 @@
 import json
 
-
-def hello(event, context):
-    body = {
-        "message": "Hello there!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-
 def public(event, context):
     body = {
         "message": "Hello World!"
@@ -40,11 +25,4 @@ def private(event, context):
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    
