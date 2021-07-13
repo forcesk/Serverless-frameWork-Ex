29e7cc274c58f5bf0f9189fb950e70dc8c525197
Emmanuel Castillo <emm.castillog@gmail.com>
2021-07-13 04:53:13 -0500

    se agregó una segunda función al handler para el endpoint /dev/api/public

--- handler.py
+++ handler.py
@@ -14,6 +14,19 @@ def hello(event, context):
 
     return response
 
+
+def public(event, context):
+    body = {
+        "message": "Hello World!"
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
     # Use this code if you don't use the http event with the LAMBDA-PROXY
     # integration
     """
