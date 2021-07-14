f7655992e6b28ebad50ccb75819f0fccdf75a116
Emmanuel Castillo <emm.castillog@gmail.com>
2021-07-13 19:49:41 -0500

    el authorizer ya detecta el jwt

--- authorizer.py
+++ authorizer.py
@@ -1,17 +1,24 @@
 import json
 import os
 import requests
+from get_token import get_token
 
 def authorizerFunc(event, context):
-    id = '2'
-    print(id)
 
-    if id == '2' :
+    id = 1
+    token = get_token(event)
+    print('Token value: '+str(event))
+
+    if token == 0:
+        id = 2
+
+
+    if id == 1 :
         policy = generate_policy(id,'Allow',event['methodArn'])
         return policy
 
     else:
-        policy = generate_policy(id,"Deny",event['methodArn'])
+        policy = generate_policy(id,'Deny',event['methodArn'])
         return policy
 
 def generate_policy(principal_id, effect,resource, scopes=None):
